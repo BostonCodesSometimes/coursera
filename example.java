c9fa583632c84a9e715795c1b4f03a194c64ec00
bbailey39 <bbailey39@huskers.unl.edu>
2025-09-09 21:29:35 -0500

    thing

---
+++ example.java
@@ -0,0 +1,9 @@
+public class example {
+    public static void sayHello() {
+        System.out.println("wassup");
+    }
+
+    public static void main(String[] args) {
+        sayHello(); // Call the method
+    }
+}
